61f8850e5dc2127aa203eab60649624db801b9f0
Zulfabasmallahagustyn <zulfaba15@gmail.com>
2023-05-22 19:01:18 +0700

    first commit

--- deployData.js
+++ deployData.js
@@ -6,10 +6,10 @@
   NetlifyDeployUrl: berisi string url Netlify deployment
 */
 
-const studentName = 'TODO: replace me'; // TODO: replace this
+const studentName = 'Zulfa Basallah Agustyn'; // TODO: replace this
 
-const studentId = 'TODO: replace me'; // TODO: replace this
+const studentId = 'FE5594086'; // TODO: replace this
 
-const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+const NetlifyDeployUrl = 'https://zulfaba15.netlify.app'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
